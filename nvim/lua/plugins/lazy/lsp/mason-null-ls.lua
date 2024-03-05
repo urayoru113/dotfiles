ef8a4959c5d7d7565848aa68feb921bb7ac5d86b
urayoru113 <yangvincent@synology.com>
2024-03-05 13:40:20 +0800

    Add .bashrc and rewrite mason-null-ls.lua

--- nvim/lua/plugins/lazy/lsp/mason-null-ls.lua
+++ nvim/lua/plugins/lazy/lsp/mason-null-ls.lua
@@ -1,36 +1,32 @@
 local options = function()
-	return {
-		handlers = { function() end },
-	}
+  return {
+    handlers = {},
+  }
 end
 
 local spec = {
-	"jay-babu/mason-null-ls.nvim",
-	event = { "BufReadPre", "BufNewFile" },
-	opts = options,
-	init = function()
-		require("core.utils").load_mappings("lsp")
-	end,
-	dependencies = {
-		"nvim-lua/plenary.nvim",
-		"williamboman/mason.nvim",
-		"nvimtools/none-ls.nvim",
-	},
-	config = function(_, opts)
-		local null_ls = require("null-ls")
-		require("mason-null-ls").setup(opts)
-		null_ls.setup({
-			sources = {
-				null_ls.builtins.diagnostics.sqlfluff.with({
-					extra_args = { "--dialect", "mysql", "--exclude-rules", "LT02,LT09" },
-				}),
-				-- null_ls.builtins.formatting.standardjs,
-				null_ls.builtins.code_actions.eslint_d,
-				null_ls.builtins.diagnostics.eslint_d,
-				null_ls.builtins.formatting.eslint_d,
-			},
-		})
-	end,
+  "jay-babu/mason-null-ls.nvim",
+  event = { "BufReadPre", "BufNewFile" },
+  opts = options,
+  init = function()
+    require("core.utils").load_mappings("lsp")
+  end,
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "williamboman/mason.nvim",
+    "nvimtools/none-ls.nvim",
+  },
+  config = function(_, opts)
+    local null_ls = require("null-ls")
+    require("mason-null-ls").setup(opts)
+    null_ls.setup({
+      sources = {
+        null_ls.builtins.diagnostics.sqlfluff.with({
+          extra_args = { "--dialect", "mysql", "--exclude-rules", "LT02,LT09" },
+        }),
+      },
+    })
+  end,
 }
 
 return spec
