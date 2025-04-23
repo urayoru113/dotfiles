151434178c48b09a8ed3489ff4e1dfb26c65efad
urayoru113 <yangvincent@synology.com>
2025-04-23 18:52:48 +0800

    feat: Add new autopair plugin and flesh.lua

--- nvim/lua/plugins/lazy/nvim-autopairs.lua
+++ nvim/lua/plugins/lazy/nvim-autopairs.lua
@@ -6,33 +6,32 @@ local options = {
 }
 
 local spec = {
-  {
-    "windwp/nvim-autopairs",
-    opts = options,
-    config = function(_, opts)
-      local npairs = require("nvim-autopairs")
-      npairs.setup(opts)
+  cond = false,
+  "windwp/nvim-autopairs",
+  opts = options,
+  config = function(_, opts)
+    local npairs = require("nvim-autopairs")
+    npairs.setup(opts)
 
-      local rule = require("nvim-autopairs.rule")
-      local cond = require("nvim-autopairs.conds")
-      local ts_cond = require("nvim-autopairs.ts-conds")
+    local rule = require("nvim-autopairs.rule")
+    local cond = require("nvim-autopairs.conds")
+    local ts_cond = require("nvim-autopairs.ts-conds")
 
-      npairs.add_rules({
-        rule("%", "%", "htmldjango"):with_pair(cond.before_text("{")),
-      })
-      npairs.add_rules({
-        rule(" then", "end", "lua"):end_wise(function()
-          return ts_cond.is_ts_node({ "if_statement" })
-        end),
-        rule(" do", "end", "lua"):end_wise(function()
-          return ts_cond.is_ts_node({ "for_statement" })
-        end),
-        rule(")", "end", "lua"):end_wise(function()
-          return ts_cond.is_ts_node({ "function" })
-        end),
-      })
-    end,
-  },
+    npairs.add_rules({
+      rule("%", "%", "htmldjango"):with_pair(cond.before_text("{")),
+    })
+    npairs.add_rules({
+      rule(" then", "end", "lua"):end_wise(function()
+        return ts_cond.is_ts_node({ "if_statement" })
+      end),
+      rule(" do", "end", "lua"):end_wise(function()
+        return ts_cond.is_ts_node({ "for_statement" })
+      end),
+      rule(")", "end", "lua"):end_wise(function()
+        return ts_cond.is_ts_node({ "function" })
+      end),
+    })
+  end,
 }
 
 return spec
