3e89360367f4e2f50636be87d57b5fca75dc577e
urayoru113 <v3691261@gmail.com>
2025-12-05 04:37:27 +0800

    feat: Add home.nix and update flake.nix

--- flake.nix
+++ flake.nix
@@ -1,19 +1,39 @@
 {
   description = "A reproducible development environment with common dev tools.";
-
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11"; 
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs-stable.url = "github:NixOS/nixpkgs/nixos-25.05";
+    home-manager = {
+      url = "github:nix-community/home-manager/release-25.05";
+      inputs.nixpkgs.follows = "nixpkgs-stable";
+    };
   };
 
-  outputs = { self, nixpkgs, ... }: {
-    devShells.default = nixpkgs.lib.makeSimplerShell {
-      packages = with nixpkgs.legacyPackages.x86_64-linux; [ 
-        git         
-        curl        
-        nodejs_24
-        neovim      
-        rustup      
-      ];
+  outputs = {
+    self,
+    nixpkgs-stable,
+    home-manager,
+    ...
+  }: let
+    system = "x86_64-linux";
+    pkgs = import nixpkgs-stable {inherit system;};
+    packages = with pkgs; [
+      curl
+      git
+      jq
+      nix-tree
+    ];
+  in {
+    devShells.${system}.default = pkgs.mkShell {
+      inherit packages;
+    };
+    packages.${system}.dev-tools = pkgs.buildEnv {
+      name = "dev-tools-env";
+      paths = packages;
+    };
+    homeConfigurations.urayoru = home-manager.lib.homeManagerConfiguration {
+      pkgs = nixpkgs-stable.legacyPackages.x86_64-linux;
+      modules = [./home.nix];
     };
   };
 }
