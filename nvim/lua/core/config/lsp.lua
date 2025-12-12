66b8b1445ed743bf61fe97725eebc76bd73de1e6
urayoru113 <v3691261@gmail.com>
2025-12-13 03:19:18 +0800

    fix: fix ruff lint didn't work

--- nvim/lua/core/config/lsp.lua
+++ nvim/lua/core/config/lsp.lua
@@ -150,74 +150,82 @@ M.providers = {
     on_attach = function(client)
       client.server_capabilities.hoverProvider = false
     end,
-    settings = {
-      --configuration = vim.fn.expand("%:p:h") .. "pyproject.toml",
-      configurationPreference = "filesystemFirst",
-      lineLength = 120,
-      lint = {
-        extendSelect = {
-          -- https://docs.astral.sh/ruff/rules/
-          "ASYNC", -- flake8-async
-          "B",     -- flake8-bugbear
-          "C4",    -- flake8-comprehensions
-          "C90",   -- mccabe
-          "COM",   -- flake8-commas
-          "D",     -- pydocstyle
-          "DTZ",   -- flake8-datetimez
-          "E",     -- pycodestyle Error
-          "F",     -- pyflakes
-          "FLY",   -- flynt
-          "G",     -- flake8-logging-format
-          "I",     -- isort
-          "ISC",   -- flake8-implicit-str-concat
-          "PIE",   -- flake8-pie
-          "PLC",   -- Pylint Convention
-          "PLE",   -- Pylint Error
-          "PLW",   -- Pylint Warning
-          "RET",   -- flake8-return
-          "RUF",   -- Ruff-specific rules
-          "RSE",   -- flake8-raise
-          "SIM",   -- flake8-simplify
-          "TC",    -- flake8-type-checking
-          "TID",   -- flake8-tidy-imports
-          "UP",    -- pyupgrade
-          "W",     -- pycodestyle Warning
-          "YTT",   -- flake8-2020
+    init_options = {
+      settings = {
+        --configuration = vim.fn.expand("%:p:h") .. "pyproject.toml",
+        configurationPreference = "filesystemFirst",
+        lineLength = 120,
+        showSyntaxErrors = false,
+        configuration = {
+          format = {
+            ["skip-magic-trailing-comma"] = false,
+          },
         },
-        ignore = {
-          "E741", -- Ambiguous variable name isn't a huge problem
-          "B008", -- Do not perform function calls in argument defaults
-          "B011", -- Allow `assert false`. We don't use `python -O`
-          "B028", -- We don't need explicit `stacklevel` for warnings
-          "D100", -- Docstring stuff
-          "D101",
-          "D102",
-          "D104",
-          "D105",
-          "D107",
-          "D203",
-          "D213",
-          "D401",
-          "D402",
-          "DTZ005", -- Allow `datetime.datetime.now()` to be called without a `tz` argument
-          "E402",   -- Simplified black ignore config to make VSCode happy
-          "E501",
-          "E701",
-          "E731",
-          "C408",
-          "E203",
-          "G004",    -- Ignore `Logging statement uses f-string` warning
-          "RET505",  -- Allow unnecessary `elif` after `return` statement
-          "D106",    -- Allow undocumented public nested class
-          "D205",    -- No blank line required after summary
-          "PLW2901", -- Allow overwriting variables in loops ,
-        },
-        unfixable = {
-          "F401",
-          "F841", -- Unused local variable
-          "F601", -- Dictionary key literal repeated
-          "F602", -- Dictionary key repeated
-          "B018", -- Useless expression
+        lint = {
+          extendSelect = {
+            -- https://docs.astral.sh/ruff/rules/
+            "ASYNC", -- flake8-async
+            "B",     -- flake8-bugbear
+            "C4",    -- flake8-comprehensions
+            "C90",   -- mccabe
+            "COM",   -- flake8-commas
+            "D",     -- pydocstyle
+            "DTZ",   -- flake8-datetimez
+            "E",     -- pycodestyle Error
+            "F",     -- pyflakes
+            "FLY",   -- flynt
+            "G",     -- flake8-logging-format
+            "I",     -- isort
+            "ISC",   -- flake8-implicit-str-concat
+            "PIE",   -- flake8-pie
+            "PLC",   -- Pylint Convention
+            "PLE",   -- Pylint Error
+            "PLW",   -- Pylint Warning
+            "RET",   -- flake8-return
+            "RUF",   -- Ruff-specific rules
+            "RSE",   -- flake8-raise
+            "SIM",   -- flake8-simplify
+            "TC",    -- flake8-type-checking
+            "TID",   -- flake8-tidy-imports
+            "UP",    -- pyupgrade
+            "W",     -- pycodestyle Warning
+            "YTT",   -- flake8-2020
+          },
+          ignore = {
+            "E741", -- Ambiguous variable name isn't a huge problem
+            "B008", -- Do not perform function calls in argument defaults
+            "B011", -- Allow `assert false`. We don't use `python -O`
+            "B028", -- We don't need explicit `stacklevel` for warnings
+            "D100", -- Docstring stuff
+            "D101",
+            "D102",
+            "D104",
+            "D105",
+            "D107",
+            "D203",
+            "D213",
+            "D401",
+            "D402",
+            "DTZ005", -- Allow `datetime.datetime.now()` to be called without a `tz` argument
+            "E402",   -- Simplified black ignore config to make VSCode happy
+            "E501",
+            "E701",
+            "E731",
+            "C408",
+            "E203",
+            "G004",    -- Ignore `Logging statement uses f-string` warning
+            "RET505",  -- Allow unnecessary `elif` after `return` statement
+            "D106",    -- Allow undocumented public nested class
+            "D205",    -- No blank line required after summary
+            "PLW2901", -- Allow overwriting variables in loops ,
+          },
+          unfixable = {
+            "F401",
+            "F841", -- Unused local variable
+            "F601", -- Dictionary key literal repeated
+            "F602", -- Dictionary key repeated
+            "B018", -- Useless expression
+          },
         },
       },
     },
