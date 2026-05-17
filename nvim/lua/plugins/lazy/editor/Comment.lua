82dc0ac72dd1e27595dc366ac88e03927464a4f2
urayoru113 <v3691261@gmail.com>
2026-05-18 00:40:01 +0800

    feat: Update Comment with <C-/> and <M-_>

--- nvim/lua/plugins/lazy/editor/Comment.lua
+++ nvim/lua/plugins/lazy/editor/Comment.lua
@@ -2,14 +2,20 @@ return
 {
   -- https://github.com/numToStr/Comment.nvim
   -- add this to your lua/plugins.lua, lua/plugins/init.lua,  or the file you keep your other plugins:
-  'numToStr/Comment.nvim',
+  "numToStr/Comment.nvim",
   opts = {
     -- add any options here
     toggler = {
       ---Line-comment toggle keymap
-      line = '<C-_>',
+      line = "<C-_>",
       ---Block-comment toggle keymap
-      block = '<leader>/',
+      block = "<M-/>",
+    },
+    opleader = {
+      ---Line-comment keymap
+      line = "<C-_>",
+      ---Block-comment keymap
+      block = "<M-/>",
     },
     mappings = {
       ---Operator-pending mapping; `gcc` `gbc` `gc[count]{motion}` `gb[count]{motion}`
