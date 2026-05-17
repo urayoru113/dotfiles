385ccb4c7fb13ed75a21c4da28146dcecd4c72f1
urayoru113 <v3691261@gmail.com>
2026-05-18 04:28:02 +0800

    chore: Update dap-view version to 1.*

--- nvim/lua/plugins/lazy/lsp/dap.lua
+++ nvim/lua/plugins/lazy/lsp/dap.lua
@@ -3,51 +3,52 @@ local autocmds = require("core.autocmds")
 local dap_config = require("plugins.config.dap")
 
 local specs = {
-	{
-		enabled = dap_config.viewer == "dap-ui",
-		"rcarriga/nvim-dap-ui",
-		dependencies = {
-			"mfussenegger/nvim-dap",
-			"nvim-neotest/nvim-nio",
-		},
-		opts = {
-			force_buffers = true,
-		},
-	},
-	{
-		"theHamsta/nvim-dap-virtual-text",
-		lazy = true,
-		opts = {
-			only_first_definition = false,
-		},
-	},
-	{
-		"igorlfs/nvim-dap-view",
-		enabled = dap_config.viewer == "dap-view",
-		lazy = true,
-		init = function()
-			utils.load_autocmds("DapView", autocmds["dap-view"])
-		end,
-		opts = {
-			follow_tab = true,
-			windows = {
-				height = 0.2,
-				position = "below",
-			},
-		},
-	},
-	{
-		enabled = false,
-		"mfussenegger/nvim-dap-python",
-		dependencies = {
-			"mfussenegger/nvim-dap",
-		},
-		config = function()
-			local dap_python = require("dap-python")
-			dap_python.setup(utils.get_project_python_path())
-			dap_python.test_runner = "pytest"
-		end,
-	},
+  {
+    enabled = dap_config.viewer == "dap-ui",
+    "rcarriga/nvim-dap-ui",
+    dependencies = {
+      "mfussenegger/nvim-dap",
+      "nvim-neotest/nvim-nio",
+    },
+    opts = {
+      force_buffers = true,
+    },
+  },
+  {
+    "theHamsta/nvim-dap-virtual-text",
+    lazy = true,
+    opts = {
+      only_first_definition = false,
+    },
+  },
+  {
+    "igorlfs/nvim-dap-view",
+    enabled = dap_config.viewer == "dap-view",
+    version = "1.*",
+    lazy = true,
+    init = function()
+      utils.load_autocmds("DapView", autocmds["dap-view"])
+    end,
+    opts = {
+      follow_tab = true,
+      windows = {
+        size = 0.2,
+        position = "below",
+      },
+    },
+  },
+  {
+    enabled = false,
+    "mfussenegger/nvim-dap-python",
+    dependencies = {
+      "mfussenegger/nvim-dap",
+    },
+    config = function()
+      local dap_python = require("dap-python")
+      dap_python.setup(utils.get_project_python_path())
+      dap_python.test_runner = "pytest"
+    end,
+  },
 }
 
 return specs
