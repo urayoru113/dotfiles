b67d69888e7aac4235da7b32755d1094065aa0d1
urayoru113 <v3691261@gmail.com>
2026-05-24 03:56:39 +0800

    feat: add neotest configuration

---
+++ nvim/lua/plugins/lazy/utility/neotest.lua
@@ -0,0 +1,26 @@
+return {
+  "nvim-neotest/neotest",
+  dependencies = {
+    "nvim-neotest/nvim-nio",
+    "nvim-lua/plenary.nvim",
+    "antoinemadec/FixCursorHold.nvim",
+    "nvim-treesitter/nvim-treesitter",
+    "nvim-neotest/neotest-python",
+    "nvim-neotest/neotest-plenary",
+  },
+  keys = {
+    { "<leader>tt", function() require("neotest").run.run() end, desc = "Run nearest test" },
+    { "<leader>tf", function() require("neotest").run.run(vim.fn.expand("%")) end, desc = "Run file tests" },
+    { "<leader>ts", function() require("neotest").summary.toggle() end, desc = "Toggle summary" },
+  },
+  config = function()
+    require("neotest").setup({
+      adapters = {
+        require("neotest-python")({
+          dap = { justMyCode = false },
+        }),
+        require("neotest-plenary"),
+      },
+    })
+  end,
+}
